f9b1e14a9829fd7b7b9a57de8509348ad896b050
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-05-10 22:53:33 -0300

    Update ServerTest.py

--- test/ServerTest/ServerTest.py
+++ test/ServerTest/ServerTest.py
@@ -1,4 +1,5 @@
 # Testando as APIs dos Servidores:
+import os
 import requests
 
 data_post = {
@@ -18,7 +19,15 @@ data_delete = {
     "vehicleID": 12
 }
 
-response = requests.post('http://localhost:64352/reservation', json=data_post)
-response = requests.get('http://localhost:64352/reservation', json=data_get)
-response = requests.delete('http://localhost:64352/reservation', json=data_delete)
+ECOCHARGE_SERVER_IP = os.environ.get('ECOCHARGE_SERVER_IP')
+EFLUX_SERVER_IP = os.environ.get('EFLUX_SERVER_IP')
+VOLTPOINT_SERVER_IP = os.environ.get('VOLTPOINT_SERVER_IP')
+
+response = requests.post(f'http://{VOLTPOINT_SERVER_IP}:64123/reservation', json=data_post)
+print(response.json())
+
+response = requests.get(f'http://{VOLTPOINT_SERVER_IP}:64123/reservation', params=data_get)
+print(response.json())
+
+response = requests.delete(f'http://{VOLTPOINT_SERVER_IP}:64123/reservation', json=data_delete)
 print(response.json())
