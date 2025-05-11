717112622beceb05fc717aa6c40a71e3b2c69b85
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-05-10 21:02:14 -0300

    Update Server.py

--- app/voltpoint/Server.py
+++ app/voltpoint/Server.py
@@ -11,11 +11,28 @@ reservations = ReservationsFile()
 # Criando a Aplicação Flask:
 app = Flask(__name__) # "__name__" se tornará "__main__" ao executar.
 
-# Criando a Rota para Ler as Reservas de um Veículo Específico:
+# Criando a Rota, Solicitada por Outro Servidor, para Criar uma Reserva para um Veículo Específico:
+@app.route('/reservation', methods=['POST'])
+def create_reservation():
+    data = request.json # Recebendo os Dados em JSON.
+    vehicleID = data.get('vehicleID') # Salvando o ID do Veículo, Recebido pelo JSON.
+    actualBatteryPercentage = data.get('actualBatteryPercentage') # Salvando a Porcentagem de Bateria do Veículo, Recebido pelo JSON.
+    batteryCapacity = data.get('batteryCapacity') # Salvando a Capacidade Total de Bateria do Veículo em kWh, Recebido pelo JSON.
+    print(f"\nDados Recebidos para Reserva: {data}\n")
+
+    chargingStationID = 1 # Precisa criar a lógica de escolha do posto.
+    chargingPointID = 1 # Precisa criar a lógica de escolha do ponto.
+    
+    createdReservation = reservations.createReservation(chargingStationID, chargingPointID, vehicleID, actualBatteryPercentage, batteryCapacity)
+    return jsonify(createdReservation)
+
+# Criando a Rota, Solicitada por Outro Servidor, para Ler as Reservas de um Veículo Específico:
 @app.route('/reservation', methods=['GET'])
 def read_reservations():
-    vehicleID = request.args.get('vehicleID') # Salvando o ID Recebido com Parâmetro.
-    foundedReservations = reservations.findReservation(int(vehicleID)) # Procurando pelas Reservas do Veículo.
+    data = request.json # Recebendo os Dados em JSON.
+    vehicleID = data.get('vehicleID') # Salvando o ID do Veículo, Recebido pelo JSON.
+    print(f"\nDados Recebidos para Reserva: {data}\n")
+    foundedReservations = reservations.findReservations(vehicleID) # Procurando pelas Reservas do Veículo.
     return jsonify(foundedReservations)
 
 # Rodando o Servidor:
