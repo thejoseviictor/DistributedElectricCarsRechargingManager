773931bd06245012afc83cedc247688f9c4c7592
José Victor <143978440+thejoseviictor@users.noreply.github.com>
2025-05-14 10:25:00 -0300

    "E-Flux" Server Files

---
+++ app/eflux/ChargingStationsFile.py
@@ -0,0 +1,90 @@
+# Esta Classe é Responsável por Armazenar os Dados dos Postos de Recarga da Empresa em um Arquivo ".json" no Servidor.
+# "Charging Station" Representa as Unidades (Postos) de uma Empresa Específica.
+
+import json
+import os
+
+class ChargingStationsFile:
+    # Inicializando a Classe e seus Atributos:
+    def __init__(self, json_file="charging_stations.json"):
+        os.makedirs("data", exist_ok=True) # Criando a Pasta "data", Se Não Existir.
+        self.json_file = os.path.join("data", json_file) # Salvando o Banco de Dados na Pasta "data".
+        self.chargingStationsList = [] # Lista dos Postos de Recarga.
+        self.readChargingStations() # Recuperando os Dados do Arquivo ".json"
+
+    # Lendo os Pontos de Recarga no Arquivo ".json":
+    def readChargingStations(self):
+        if os.path.exists(self.json_file):
+            with open(self.json_file, "r", encoding="utf-8") as file:
+                self.chargingStationsList = json.load(file) # Salvando os Dados do Arquivo ".json" na Lista.
+    
+    # Procurando um Posto de Recarga Específico:
+    def findChargingStation(self, chargingStationID: int):
+        self.readChargingStations() # Atualizando a Memória de Execução Com o Banco de Dados em "charging_stations.json".
+        # Percorrendo a Lista:
+        for cs in self.chargingStationsList:
+            if cs["chargingStationID"] == chargingStationID:
+                return cs
+        else:
+            print(f"Posto de Recarga com ID '{chargingStationID}' Não Foi Encontrado!\n")
+            return None
+
+    # Listando os Postos de Recarga Cadastrados no Arquivo ".json":
+    def listChargingStations(self):
+        self.readChargingStations() # Atualizando a Memória de Execução.
+        return self.chargingStationsList
+
+    # Salvando a Lista no Arquivo ".json":
+    def saveChargingStations(self):
+        with open(self.json_file, "w", encoding="utf-8") as file:
+            json.dump(self.chargingStationsList, file, indent=4, ensure_ascii=False)
+
+    # Atualizando a Cidade de um Posto de Recarga Específico:
+    def updateChargingStation(self, chargingStationID: int, city_codename: str, city_name: str):
+        self.readChargingStations() # Atualizando a Memória de Execução.
+        for cs in self.chargingStationsList:
+            if cs["chargingStationID"] == chargingStationID:
+                cs["city_codename"] = city_codename
+                cs["city_name"] = city_name
+                self.saveChargingStations() # Salvando no Arquivo ".json".
+                print(f"A Cidade do Posto de Recarga com ID '{chargingStationID}' Foi Alterada para '{city_name}' com Sucesso!\n")
+                return True
+        # Exibindo a Mensagem de Erro:
+        print(f"Posto de Recarga com ID '{chargingStationID}' Não Foi Encontrado!\n")
+        return None
+    
+    # Gerando um ID para Novo Posto de Recarga:
+    # Os IDs Não Podem Ser Iguais.
+    # IDs Novos: Maior ID + 1.
+    def generateChargingStationID(self):
+        startID = 1 # Um ID Inicial Que Será Usado Como Comparador.
+        for cs in self.chargingStationsList:
+            # ID Maior ou Igual (Para o Primeiro ID dos Postos de Recarga):
+            if cs["chargingStationID"] >= startID:
+                startID = cs["chargingStationID"] + 1
+        return startID
+    
+    # Criando um Novo Posto de Recarga e Salvando no Arquivo ".json":
+    def createChargingStation(self, city_codename: str, city_name: str):
+        self.readChargingStations() # Atualizando a Memória de Execução.
+        chargingStationID = self.generateChargingStationID()
+        # Salvando na Lista:
+        self.chargingStationsList.append({
+            "chargingStationID": chargingStationID, 
+            "city_codename": city_codename, 
+            "city_name": city_name})
+        self.saveChargingStations() # Salvando no Arquivo ".json".
+        print(f"Posto de Recarga com ID '{chargingStationID}' em '{city_name}' Foi Salvo com Sucesso!\n")
+        return chargingStationID # Retornando o ID do Posto de Recarga Criado.
+    
+    # Removendo um Posto do Arquivo ".json":
+    def deleteChargingStation(self, chargingStationID: int):
+        self.readChargingStations() # Atualizando a Memória de Execução.
+        for cs in self.chargingStationsList:
+            if cs["chargingStationID"] == chargingStationID:
+                self.chargingStationsList.remove(cs)
+                self.saveChargingStations() # Salvando no Arquivo ".json".
+                print(f"Posto de Recarga com ID '{chargingStationID}' Foi Removido com Sucesso!\n")
+                return True
+        print(f"Posto de Recarga com ID '{chargingStationID}' Não Foi Encontrado!\n")
+        return None
---
+++ app/eflux/Server.py
@@ -0,0 +1,147 @@
+# Servidor da Empresa "E-Flux", que Atua no Estado do Pernambuco -------------------------------------------------------------------------------------------------------
+
+# Importando as Dependências:
+import os # Para Usar Variáveis de Ambiente.
+from flask import Flask, request, jsonify # Para Criar a API do Servidor e Seus End-Points.
+import threading # Para Criar Múltiplas Instâncias.
+from ReservationsFile import ReservationsFile # Que Manipula a Persistência de Dados das Reservas.
+from ChargingStationsFile import ChargingStationsFile # Que Manipula a Persistência de Dados dos Postos de Recarga.
+import ReservationHelper # Funções para Gerar Parâmetros para Reservas.
+import mqttFunctions # Função para Configurar e Inicializar o MQTT.
+from Utils import sendReservationsToOtherServers # Função Para Enviar Solicitações de Reservas Para Outros Servidores.
+
+# Salvando o Nome da Empresa:
+companyName = os.environ.get('COMPANY_NAME') # Variável de Ambiente do Docker Compose.
+
+# Salvando o IP e Porta do Servidor Desta Empresa:
+SERVER_IP = os.environ.get(f'{companyName.upper()}_SERVER_IP') # IP Definido no Docker-Compose.
+SERVER_PORT = int(os.environ.get(f'{companyName.upper()}_SERVER_PORT')) # Porta Definida no Docker-Compose.
+
+# Criando o Objeto das Reservas no Banco de Dados:
+reservationsData = ReservationsFile()
+
+# Criando o Objeto dos Postos de Recarga no Banco de Dados:
+chargingStationsData = ChargingStationsFile()
+
+# Criando a Aplicação Flask:
+app = Flask(__name__) # "__name__" se tornará "__main__" ao executar.
+
+# Rota Para Agendar as Reservas de um Veículo Específico, de Acordo com a Lista da Rota de Reservas (Servidor-Servidor):
+@app.route('/reservation', methods=['POST'])
+def createReservations():
+    # Tratando os Dados Recebidos:
+    data = request.json # Recebendo os Dados em um Dicionário: data = {vehicleID: int, actualBatteryPercentage: int, batteryCapacity: float, reservationsRoute: list}.
+    vehicleID = data.get('vehicleID') # ID do Veículo.
+    actualBatteryPercentage = data.get('actualBatteryPercentage') # Porcentagem Atual de Bateria do Veículo.
+    batteryCapacity = data.get('batteryCapacity') # Capacidade de Bateria do Veículo em kWh.
+    reservationsRoute = data.get('reservationsRoute') # A Rota das Reservas.
+
+    # Verificando Se Existem Reservas Solicitadas na Lista de Rotas para Reservas:
+    if not reservationsRoute:
+        print("Erro: A Lista da Rota das Reservas Está Vazia!\n")
+        return jsonify({"error": "A Lista da Rota das Reservas Está Vazia!"}), 400  # Erro 400: Bad Request - Dados Enviados Errados ou Incompletos.
+    
+    # Exibindo as Informações das Reservas Solicitadas:
+    print(f"Dados do Veículo '{vehicleID}' Recebidos para Reservas em:\n")
+    for city in reservationsRoute:
+        print(f"Cidade: {city['name']} | Empresa: {city['company']}\n")
+    
+    bookedReservations = [] # Onde Serão Salvas Todas as Reservas Realizadas, Deste Servidor e dos Outros, Para Serem Retornadas.
+
+    # Se a Primeira Cidade da Lista de Rotas de Reservas Não For Administrada Por Este Servidor
+    # a Lista de Reservas Será Repassada Para o Servidor Correto:
+    if reservationsRoute[0]["company"] != companyName.lower():
+        response, status_code = sendReservationsToOtherServers(data, reservationsRoute)
+        # Se Não Conseguir Reservas em Outros Servidores, Nenhum Reserva Será Realizada:
+        if 400 <= status_code < 505:
+            print(f"Erro '{status_code}': {response.get_json().get('error')}\n") # Exibindo a Mensagem de Erro Recebida.
+            # Limpando as Reservas Realizadas no Servidor Atual:
+            for rs in bookedReservations:
+                reservationsData.deleteReservation(rs["reservationID"], rs["chargingStationID"], rs["chargingPointID"], rs["vehicleID"])
+            # Retornando a Mensagem de Erro:
+            return jsonify({"error": "Não Foi Possível Conseguir Reservas nos Outros Servidores!"}), status_code
+        else:
+            bookedReservations.append(response.get_json()) # Concatenando as Reservas Realizadas nos Outros Servidores.
+            return jsonify(bookedReservations), 200 # Retornando Todas as Reservas Realizadas Com Sucesso (200).
+    
+    # Copiando as Reservas Destinadas ao Servidor Desta Empresa:
+    serverReservations = [city for city in reservationsRoute if city["company"] == f"{companyName.lower()}"] # Reservas Para Este Servidor.
+
+    # Isolando as Reservas Destinadas aos Servidores das Outras Empresas:
+    reservationsRoute = [city for city in reservationsRoute if city["company"] != f"{companyName.lower()}"] # Reservas Para os Outros Servidores.
+
+    # Verificando Se Existem Postos de Recarga Cadastrados Neste Servidor:
+    if not chargingStationsData.chargingStationsList:
+        print("Erro: Não Existem Postos de Recarga Cadastrados Neste Servidor!\n")
+        return jsonify({"error": "Não Existem Postos de Recarga Cadastrados Neste Servidor!"}), 404  # Erro 404: Not Found - Recurso Não Encontrado.
+    
+    # Realizando as Reservas Deste Servidor.
+    # Procurando os Postos de Recarga Que Atuam nas Cidades Solicitadas:
+    for cs in chargingStationsData.chargingStationsList:
+        for city in serverReservations:
+            if cs["city_codename"] == city["codename"]:
+                chargingStationID = cs["chargingStationID"] # Salvando o ID do Posto de Recarga.
+                chargingPointID = ReservationHelper.chooseChargingPoint(chargingStationID) # Procurando um Ponto de Carregamento no Posto de Recarga.
+                # Verificando Se Um Ponto de Carregamento Foi Encontrado:
+                if not chargingPointID:
+                    print("Erro: Não Existem Pontos de Carregamento Cadastrados Neste Servidor!\n")
+                    return jsonify({"error": "Não Existem Pontos de Carregamento Cadastrados Neste Servidor!"}), 404  # Erro 404: Not Found - Recurso Não Encontrado.
+                else:
+                    # Realizando uma Reserva na Cidade:
+                    currentReservation = reservationsData.createReservation(chargingStationID, chargingPointID, city["name"], city["codename"],
+                                                                            companyName, vehicleID, actualBatteryPercentage, batteryCapacity)
+                    # Verificando Se a Reserva Foi Realizada:
+                    if not currentReservation:
+                        print(f"Não Foi Possível Realizar a Reserva em '{city["name"]}' Para o Veículo '{vehicleID}'\n")
+                        return jsonify({"error": f"Não Foi Possível Realizar a Reserva em '{city["name"]}' Para o Veículo '{vehicleID}"}), 404 # Erro 404: Not Found
+                    else:
+                        bookedReservations.append(currentReservation) # Salvando a Reserva na Lista de Reservas Que Será Retornada.
+    
+    # Retornando as Reservas, Se Não Houveram Mais Reservas Para Outros Servidores:
+    if not reservationsRoute:
+        return jsonify(bookedReservations), 200 # Retornando Todas as Reservas Realizadas Com Sucesso (200).
+    
+    # Repassando as Reservas Que Sobraram Para os Outros Servidores:
+    else:
+        response, status_code = sendReservationsToOtherServers(data, reservationsRoute)
+        # Se Não Conseguir Reservas em Outros Servidores, Nenhum Reserva Será Realizada:
+        if 400 <= status_code < 505:
+            print(f"Erro '{status_code}': {response.get_json().get('error')}\n") # Exibindo a Mensagem de Erro Recebida.
+            # Limpando as Reservas Realizadas no Servidor Atual:
+            for rs in bookedReservations:
+                reservationsData.deleteReservation(rs["reservationID"], rs["chargingStationID"], rs["chargingPointID"], rs["vehicleID"])
+            # Retornando a Mensagem de Erro:
+            return jsonify({"error": "Não Foi Possível Conseguir Reservas nos Outros Servidores!"}), status_code
+        else:
+            bookedReservations.append(response.get_json()) # Concatenando as Reservas Realizadas nos Outros Servidores.
+            return jsonify(bookedReservations), 200 # Retornando Todas as Reservas Realizadas Com Sucesso (200).
+
+# Rota para Ler as Reservas de um Veículo Específico (Servidor-Servidor):
+@app.route('/reservation', methods=['GET'])
+def readReservations():
+    vehicleID = request.args.get('vehicleID') # Salvando o ID Recebido com Parâmetro.
+    print(f"ID do Veículo Recebido: {vehicleID}\n")
+    foundedReservations = reservationsData.findReservations(int(vehicleID)) # Procurando pelas Reservas do Veículo.
+    return jsonify(foundedReservations), 200 # Retornando Sucesso (200).
+
+# Rota para Excluir uma Reserva de um Veículo Específico (Servidor-Servidor):
+@app.route('/reservation', methods=['DELETE'])
+def deleteReservations():
+    data = request.json # Recebendo os Dados em JSON.
+    reservationID = data.get('reservationID') # Salvando o ID da Reserva, Recebido pelo JSON.
+    chargingStationID = data.get('chargingStationID') # Salvando o ID do Posto de Recarga, Recebido pelo JSON.
+    chargingPointID = data.get('chargingPointID') # Salvando o ID do Ponto de Carregamento, Recebido pelo JSON.
+    vehicleID = data.get('vehicleID') # Salvando o ID do Veículo, Recebido pelo JSON.
+    print(f"\nDados Recebidos para Reserva: {data}\n")
+    deleteStatus = reservationsData.deleteReservation(reservationID, chargingStationID, chargingPointID, vehicleID)
+    return jsonify(deleteStatus), 200 # Retornando Sucesso (200).
+
+# Rodando o Servidor no IP da Máquina:
+if __name__ == '__main__':
+    # Iniciando o MQTT em Outra Thread:
+    mqtt_thread = threading.Thread(target=mqttFunctions.startMQTT) # Configurando a Thread do MQTT.
+    mqtt_thread.daemon = True # Thread "Daemon" Que Se Encerrará Junto Com o Servidor.
+    mqtt_thread.start() # Iniciando a Thread do MQTT.
+
+    # Iniciando o Servidor HTTP (Flask):
+    app.run(host=SERVER_IP, port=SERVER_PORT, debug=True, threaded=True) # Threaded = O Flask Pode Tratar Múltiplas Requisições Ao Mesmo Tempo.
